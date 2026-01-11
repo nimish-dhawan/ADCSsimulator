715c78f0ac261fc9cb095190c2e5346a0600f300
nimish-dhawan <81336851+nimish-dhawan@users.noreply.github.com>
2026-01-10 23:34:40 -0500

    14.5.1
    Fixed TRIAD output, cleaned up MEKF, cleaned up continuity.m

--- Misc/continuity.m
+++ Misc/continuity.m
@@ -1,8 +1,21 @@
-function q_smooth = continuity(q, q_prev)
+function q_smooth = continuity(q, q_ini)
 
-    if dot(q, q_prev) < 0
-        q_smooth = -q;
-    else
-        q_smooth = q;
-    end
+persistent q_prev
+
+if nargin <2
+    q_ini = [0; 0; 0; 1];
+end
+
+if isempty(q_prev)
+    q_prev = q_ini;
+end
+
+if dot(q, q_prev) < 0
+    q_smooth = -q;
+else
+    q_smooth = q;
+end
+
+q_prev = q_smooth;
+
 end
--- Misc/mekf.m
+++ Misc/mekf.m
@@ -1,21 +1,43 @@
-function [q_plus, b_plus, P_plus] = mekf(q_pre, w_meas, b_pre, dt, s_I, ...
-                                b_I, s_B, b_B, R, sigma_g, sigma_b, P_pre)
 % Nimish Dhawan 2025
-
+% MULTIPLICATIVE EXTENDED KALMAN FILTER
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input(s)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+% q_pre   [4x1]: Previous step postiori quaternion estimate
+% w_meas  [3x1]: Previous step measured rate (or current??)
+% dt      [1x1]: Discrete time step
+% s_I     [3x1]: Calculated Sun direction vector (ECI)
+% s_B     [3x1]: Measured Sun direction vector (Body fixed)
+% b_I     [3x1]: Calculated magnetic field vector (ECI)
+% b_B     [3x1]: Measured magnetic field vector (Body fixed)
+% R       [6x6]: Measurement covariance matrix
+% sigma_g [1x1]: Random walk covariance
+% sigma_b [1x1]: Bias covariance
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Output(s)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% q_plus [4x1]: Current step postiori quaternion estimate
+% w_plus [3x1]: Current step postiori angular rate estimate
 
-tilde  = @(E, Eta) [Eta*eye(3)-skew(E), E; -E', Eta];
-qBox   = @(E, Eta) [Eta*eye(3)+skew(E), E; -E', Eta];
-qDot   = @(q, w) 0.5 * qBox(q(1:3), q(4)) * [w(:); 0];
-quat2C = @(E, Eta) (Eta^2-E'*E)*eye(3) + 2*(E*E') - 2*Eta*skew(E);
+function [q_plus, w_plus] = mekf(q_pre, w_meas, dt, s_I, ...
+                                b_I, s_B, b_B, R, sigma_g, sigma_b)
 
+persistent bias P
+
+if isempty(bias)
+    bias = 1e-09*ones(3,1);
+end
+
+if isempty(P)
+    P = 0.000001*eye(6);
+end
+
+% Helper functions
+quat2C = @(q) (q(4)^2-q(1:3)'*q(1:3))*eye(3) + 2*(q(1:3)*q(1:3)')...
+               - 2*q(4)*skew(q(1:3));
+
+% Forcing unit vectors
 s_I = s_I/norm(s_I);
 s_B = s_B/norm(s_B);
 b_I = b_I/norm(b_I);
@@ -23,53 +45,37 @@ b_B = b_B/norm(b_B);
 
 % ========================================================================
 % Propagation
-w_hat = w_meas - b_pre;
+
+% Initializing
+w_hat = w_meas - bias;
+w_hat = w_hat(:);
 w_hat = w_hat(1:3);
-db    = zeros(3,1);          % Mean is zero, covariance accounted for in Q
-
-% phi = norm(w_hat)*dt;
-% phi = (w_hat)*dt/100;
-phi = zeros(3,1);
-dq  = [0.5*phi; 1];
-
-% q_minus is quat multipication of q_predicted using q_pre
-k1 = qDot(q_pre          , w_hat);
-k2 = qDot(q_pre+0.5*k1*dt, w_hat);
-k3 = qDot(q_pre+0.5*k2*dt, w_hat);
-k4 = qDot(q_pre+k3*dt    , w_hat);
-
-q_pred = q_pre + (dt/6)*(k1+2*k2+2*k3+k4); 
-q_pred = q_pred/norm(q_pred);
-
-% Priori Estimates
-q_minus = tilde(dq(1:3), dq(4)) * q_pred;
-q_minus = q_minus/norm(q_minus);
-b_minus = b_pre + db;
-
 theta = norm(w_hat) * dt;
+
+% Priori estimates using Eqs. (7.53)-(7.60) given by Crassidis and Junkins
+psi     = sin(0.5*theta)*w_hat / norm(w_hat);
+OMEGA   = [cos(0.5*theta)*eye(3)-skew(psi), psi; -psi', cos(0.5*theta)];
+q_minus = OMEGA*q_pre;
+q_minus = q_minus/norm(q_minus);
+b_minus = bias;
 
 Phi11 = eye(3) - (1/norm(w_hat))*skew(w_hat)*sin(theta) + ...
         (1/norm(w_hat))^2*skew(w_hat)^2*(1-cos(theta));
-Phi12 = -eye(3)*dt - (1/norm(w_hat))^2*skew(w_hat)^3*(theta-sin(theta)) + ...
-        (1/norm(w_hat))*skew(w_hat)^2*(1-cos(theta));
+Phi12 = -eye(3)*dt - (1/norm(w_hat))^2*skew(w_hat)^3*(theta-sin(theta))...
+         + (1/norm(w_hat))*skew(w_hat)^2*(1-cos(theta));
 Phi   = [Phi11, Phi12; zeros(3,3), eye(3,3)];
 
-% Q11 = (sigma_g^2*dt + sigma_b^2*dt^3/3) * eye(3);
-% Q12 = -(sigma_b^2*dt^2/2) * eye(3);
-% Q22 = sigma_b^2*dt * eye(3);
-% Q   = [Q11, Q12; Q12, Q22];
+Q11 = (sigma_g^2*dt + (sigma_b^2*dt^3)/3) * eye(3);
+Q12 = -(sigma_b^2*dt^2/2) * eye(3);
+Q22 = sigma_b^2*dt * eye(3);
+Q   = [Q11, Q12; Q12, Q22];
+G   = [-eye(3), zeros(3); zeros(3), eye(3)];
 
-G  = [-eye(3), zeros(3);
-      zeros(3), eye(3)];
-Qc = blkdiag((sigma_g^2)*eye(3), (sigma_b^2)*eye(3));
-Q  = G*Qc*G'*dt; 
-
-% Priori Covariance
-P_minus = Phi*P_pre*Phi' + Q;
+P_minus = Phi*P*Phi' + G*Q*G';
 
 % ========================================================================
 % Correction
-C_BI = quat2C(q_minus(1:3), q_minus(4));
+C_BI = quat2C(q_minus);
 
 % Predicted Measurement
 h = [C_BI*s_I;
@@ -84,8 +90,6 @@ z = [s_B;
      b_B];
 
 % Residual
-% r = [skew(z(1:3))*h(1:3);
-%      skew(z(4:6))*h(4:6)];
 r = z - h;
 
 % Kalman Gain
@@ -94,17 +98,17 @@ K = (P_minus * H') / S;
 
 dx_plus     = K*r;
 dth_plus    = dx_plus(1:3);
-dq_plus     = [0.5*dth_plus; 1];
-dq_plus     = dq_plus / norm(dq_plus);
 db_plus     = dx_plus(4:6);
 
-P_plus = P_minus * (eye(6) - K*H);
-% P_plus = (eye(6) - K*H) * P_minus * (eye(6) - K*H)' + K*R*K';
+P = P_minus * (eye(6) - K*H);
+% P = (eye(6) - K*H) * P_minus * (eye(6) - K*H)' + K*R*K';
 
+% ========================================================================
 % Reset
-q_plus = tilde(dq_plus(1:3), dq_plus(4)) * q_minus;
+q_plus = q_minus + 0.5 *...
+    [q_minus(4)*eye(3)+skew(q_minus(1:3)); -q_minus(1:3)'] * dth_plus;
 q_plus = q_plus / norm(q_plus);
-b_plus = b_minus + db_plus;
-
+bias = b_minus + db_plus;
+w_plus = w_meas - bias;
 
 end
