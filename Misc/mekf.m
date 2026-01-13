2c7510fd0b8270c23ee7a1b6747008963e1c2138
nimish-dhawan <81336851+nimish-dhawan@users.noreply.github.com>
2026-01-13 16:44:29 -0500

    v15.5.3
    Removed feedback loop from MEKF.
    Open issues:
    (1) B-dot is unstable
     (2) Controller is very sensitive, pick better gains.

--- Misc/mekf.m
+++ Misc/mekf.m
@@ -3,7 +3,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Input(s)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% q_pre   [4x1]: Previous step postiori quaternion estimate
+% q_triad [4x1]: TRIAD attitude determination
 % w_meas  [3x1]: Previous step measured rate (or current??)
 % dt      [1x1]: Discrete time step
 % s_I     [3x1]: Calculated Sun direction vector (ECI)
@@ -17,19 +17,18 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Output(s)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% q_plus [4x1]: Current step postiori quaternion estimate
-% w_plus [3x1]: Current step postiori angular rate estimate
+% q_est [4x1]: Current step postiori quaternion estimate
+% w_est [3x1]: Current step postiori angular rate estimate
 
-function [q_plus, w_plus] = mekf(q_pre, w_meas, dt, s_I, ...
+function [q_est, w_est] = mekf(q_triad, w_meas, dt, s_I, ...
                                 b_I, s_B, b_B, R, sigma_g, sigma_b)
 
-persistent bias P
+persistent bias P q
 
-if isempty(bias)
+% Initializing parameters
+if isempty(q)
+    q    = q_triad;
     bias = 1e-05*ones(3,1);
-end
-
-if isempty(P)
     P = 0.000001*eye(6);
 end
 
@@ -51,39 +50,35 @@ w_hat = w_meas - bias;
 w_hat = w_hat(:);
 w_hat = w_hat(1:3);
 
-wn = norm(w_hat);
-W  = skew(w_hat);
+wn    = norm(w_hat);
+w_sk  = skew(w_hat);
 
 % Priori estimates using Eqs. (7.53)-(7.60) given by Crassidis and Junkins
 if wn < 1e-8
     % Small angle assumption
     psi = 0.5*dt*w_hat;
-    OMEGA = [eye(3)-skew(psi), psi;
-            -psi',            1];
+    OMEGA = [eye(3)-skew(psi), psi; -psi', 1];
 else
     theta = wn*dt;
     psi   = sin(0.5*theta)*w_hat/wn;
     OMEGA = [cos(0.5*theta)*eye(3)-skew(psi), psi; -psi', cos(0.5*theta)];
 end
 
-q_minus = OMEGA*q_pre;
+q_minus = OMEGA*q;
 q_minus = q_minus/norm(q_minus);
 b_minus = bias;
 
 if wn < 1e-8
     % Small angle assumption
-    Phi11 = eye(3) - W*dt;
+    Phi11 = eye(3) - w_sk*dt;
     Phi12 = -eye(3)*dt;
 else
     theta = wn*dt;
-    Phi11 = eye(3) - (W/wn)*sin(theta) + (W*W/wn^2)*(1-cos(theta));
-    Phi12 = -eye(3)*dt - (W*W/wn^3)*(theta-sin(theta)) + (W/wn^2)*(1-cos(theta));
+    Phi11 = eye(3) - (w_sk/wn)*sin(theta) + ...
+            (w_sk*w_sk/wn^2)*(1-cos(theta));
+    Phi12 = -eye(3)*dt - (w_sk*w_sk/wn^3)*(theta-sin(theta)) ...
+            + (w_sk/wn^2)*(1-cos(theta));
 end
-
-% Phi11 = eye(3) - (1/norm(w_hat))*skew(w_hat)*sin(theta) + ...
-%         (1/norm(w_hat))^2*skew(w_hat)^2*(1-cos(theta));
-% Phi12 = -eye(3)*dt - (1/norm(w_hat))^3*skew(w_hat)^2*(theta-sin(theta))...
-%          + (1/norm(w_hat))^2*skew(w_hat)*(1-cos(theta));
 
 Phi   = [Phi11, Phi12; zeros(3,3), eye(3,3)];
 
@@ -127,10 +122,12 @@ P = P_minus * (eye(6) - K*H);
 
 % ========================================================================
 % Reset
-q_plus = q_minus + 0.5 *...
+q = q_minus + 0.5 *...
     [q_minus(4)*eye(3)+skew(q_minus(1:3)); -q_minus(1:3)'] * dth_plus;
-q_plus = q_plus / norm(q_plus);
-bias = b_minus + db_plus;
-w_plus = w_meas - bias;
+q = q / norm(q);
+
+q_est = q;
+bias   = b_minus + db_plus;
+w_est = w_meas - bias;
 
 end
