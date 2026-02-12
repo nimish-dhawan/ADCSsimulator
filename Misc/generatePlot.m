a922d684fdd33e5d8a173a436a51de8f1dcda54c
nimish-dhawan <81336851+nimish-dhawan@users.noreply.github.com>
2026-02-12 11:30:11 -0500

    v16.2
    Made improvements to the app:
    - User can now save and load GUI states
    - Improved interface for configuring CoM and PD controller gains
    - Improved interface for generating MATLAB plots

--- Misc/generatePlot.m
+++ Misc/generatePlot.m
@@ -1,38 +1,36 @@
-% Nimish Dhawan
+% Nimish Dhawan, 2026
+
+function generatePlot(app)
+
 clc
 close all
 
-t = app.simOut.tout;
+simOut = app.simOut;
+t = simOut.tout;
 
-% Creating a root plot window
-
-if plot_graphs == true
 %%
 % Plotting angular velocity
 figure()
-formatfig()
 
 subplot(3,1,1)
-    plot(t, simOut.dataPacket.wsc1_est.Data, '-b')
+    plot(t, simOut.dataPacket.wsc1_est.Data, '-k')
     hold on
     plot(t, simOut.dataPacket.wsc1.Data, '--r')
     grid on
     xlim([t(1) t(end)])
-    legend('Estimate', 'Ground Truth')
-    xlabel('Time [s]')
+    legend('Estimate', 'Ground Truth', 'Location','best')
     ylabel('\omega_x [rad/s]')
 
 subplot(3,1,2)
-    plot(t, simOut.dataPacket.wsc2_est.Data, '-b')
+    plot(t, simOut.dataPacket.wsc2_est.Data, '-k')
     hold on
     plot(t, simOut.dataPacket.wsc2.Data, '--r')
     grid on
     xlim([t(1) t(end)])
-    xlabel('Time [s]')
     ylabel('\omega_y [rad/s]')
 
 subplot(3,1,3)
-    plot(t, simOut.dataPacket.wsc3_est.Data, '-b')
+    plot(t, simOut.dataPacket.wsc3_est.Data, '-k')
     hold on
     plot(t, simOut.dataPacket.wsc3.Data, '--r')
     grid on
@@ -40,41 +38,40 @@ subplot(3,1,3)
     xlabel('Time [s]')
     ylabel('\omega_z [rad/s]')
 theme(gcf,"light")
+formatfig(0.4,0.4)
+fontname('Times New Roman');
+fontsize(9,"points");
 
 % Plotting spacecraft attitude
 figure()
-formatfig()
 
 subplot(4,1,1)
-    plot(t, simOut.dataPacket.q1_est.Data, '-b')
+    plot(t, simOut.dataPacket.q1_est.Data, '-k')
     hold on
     plot(t, simOut.dataPacket.q1.Data, '--r')
     grid on
     xlim([t(1) t(end)])
-    legend('Estimate', 'Ground Truth')
-    xlabel('Time [s]')
+    legend('Estimate', 'Ground Truth', 'Location','best')
     ylabel('q_1')
 
 subplot(4,1,2)
-    plot(t, simOut.dataPacket.q2_est.Data, '-b')
+    plot(t, simOut.dataPacket.q2_est.Data, '-k')
     hold on
     plot(t, simOut.dataPacket.q2.Data, '--r')
     grid on
     xlim([t(1) t(end)])
-    xlabel('Time [s]')
     ylabel('q_2')
 
 subplot(4,1,3)
-    plot(t, simOut.dataPacket.q3_est.Data, '-b')
+    plot(t, simOut.dataPacket.q3_est.Data, '-k')
     hold on
     plot(t, simOut.dataPacket.q3.Data, '--r')
     grid on
     xlim([t(1) t(end)])
-    xlabel('Time [s]')
     ylabel('q_3')
 
 subplot(4,1,4)
-    plot(t, simOut.dataPacket.q4_est.Data, '-b')
+    plot(t, simOut.dataPacket.q4_est.Data, '-k')
     hold on
     plot(t, simOut.dataPacket.q4.Data, '--r')
     grid on
@@ -82,53 +79,56 @@ subplot(4,1,4)
     xlabel('Time [s]')
     ylabel('q_4')
 theme(gcf,"light")
+formatfig(0.4,0.5)
+fontname('Times New Roman');
+fontsize(9,"points");
 
 % Wheels angular momentum
 figure()
-formatfig()
+formatfig(0.4,0.4)
 
 subplot(3,1,1)
-    plot(t, simOut.dataPacket.h1_wheels.Data, '-b')
+    plot(t, simOut.dataPacket.h1_wheels.Data, '-k')
     grid on
     xlim([t(1) t(end)])
-    xlabel('Time [s]')
     ylabel('h_{b,1} [N.m.s]')
 
 subplot(3,1,2)
-    plot(t, simOut.dataPacket.h2_wheels.Data, '-b')
+    plot(t, simOut.dataPacket.h2_wheels.Data, '-k')
     grid on
     xlim([t(1) t(end)])
-    xlabel('Time [s]')
     ylabel('h_{b,2} [N.m.s]')
 
 subplot(3,1,3)
-    plot(t, simOut.dataPacket.h3_wheels.Data, '-b')
+    plot(t, simOut.dataPacket.h3_wheels.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
     ylabel('h_{b,3} [N.m.s]')
 theme(gcf,"light")
+fontname('Times New Roman');
+fontsize(9,"points");
 
 % Wheels angular velocity
 figure()
-formatfig()
+formatfig(0.4,0.4)
 
 subplot(3,1,1)
-    plot(t, simOut.dataPacket.w1_wheels.Data, '-b')
+    plot(t, simOut.dataPacket.w1_wheels.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
     ylabel('\omega_{b,1} [rad/s]')
 
 subplot(3,1,2)
-    plot(t, simOut.dataPacket.w2_wheels.Data, '-b')
+    plot(t, simOut.dataPacket.w2_wheels.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
     ylabel('\omega_{b,2} [rad/s]')
 
 subplot(3,1,3)
-    plot(t, simOut.dataPacket.w3_wheels.Data, '-b')
+    plot(t, simOut.dataPacket.w3_wheels.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
@@ -137,24 +137,24 @@ theme(gcf,"light")
 
 % Command torque
 figure()
-formatfig()
+formatfig(0.4,0.4)
 
 subplot(3,1,1)
-    plot(t, simOut.dataPacket.T1_cmd.Data, '-b')
+    plot(t, simOut.dataPacket.T1_cmd.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
     ylabel('T_{b,1} [N·m]')
 
 subplot(3,1,2)
-    plot(t, simOut.dataPacket.T2_cmd.Data, '-b')
+    plot(t, simOut.dataPacket.T2_cmd.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
     ylabel('T_{b,2} [N·m]')
 
 subplot(3,1,3)
-    plot(t, simOut.dataPacket.T3_cmd.Data, '-b')
+    plot(t, simOut.dataPacket.T3_cmd.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
@@ -163,24 +163,24 @@ theme(gcf,"light")
 
 % Command current for torque rods
 figure()
-formatfig()
+formatfig(0.4,0.4)
 
 subplot(3,1,1)
-plot(t, simOut.dataPacket.I_rod1.Data, '-b')
+plot(t, simOut.dataPacket.I_rod1.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
     ylabel('I_{rod,1} [A]')
 
 subplot(3,1,2)
-    plot(t, simOut.dataPacket.I_rod2.Data, '-b')
+    plot(t, simOut.dataPacket.I_rod2.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
     ylabel('I_{rod,2} [A]')
 
 subplot(3,1,3)
-    plot(t, simOut.dataPacket.I_rod3.Data, '-b')
+    plot(t, simOut.dataPacket.I_rod3.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
@@ -189,24 +189,24 @@ theme(gcf,"light")
 
 % Torque produced by the rod
 figure()
-formatfig()
+formatfig(0.4,0.4)
 
 subplot(3,1,1)
-plot(t, simOut.dataPacket.T_rod1.Data, '-b')
+plot(t, simOut.dataPacket.T_rod1.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
     ylabel('T_{rod,1} [N·m]')
 
 subplot(3,1,2)
-    plot(t, simOut.dataPacket.T_rod2.Data, '-b')
+    plot(t, simOut.dataPacket.T_rod2.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
     ylabel('T_{rod,2} [N·m]')
 
 subplot(3,1,3)
-    plot(t, simOut.dataPacket.T_rod3.Data, '-b')
+    plot(t, simOut.dataPacket.T_rod3.Data, '-k')
     grid on
     xlim([t(1) t(end)])
     xlabel('Time [s]')
@@ -214,14 +214,12 @@ subplot(3,1,3)
 theme(gcf,"light")
 
 
-end
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Display Output
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 fprintf('\n--- Orbit Elements ---\n');
-fprintf('a      = %.3f km\n', app.AltitudekmEditField.Value+rE);
+fprintf('a      = %.3f km\n', app.AltitudekmEditField.Value+app.rE);
 fprintf('e      = %.5f\n', app.EccentricityEditField.Value);
 fprintf('i      = %.3f deg\n', app.InclinationdegEditField.Value);
 fprintf('w      = %.3f deg\n', app.ArgumentofPergieedegEditField.Value);
@@ -230,19 +228,19 @@ fprintf('t      = %.3f sec\n', app.TimeofPerigeePassagesecEditField.Value);
 
 fprintf('\n--- Scenario Epoch ---\n');
 fprintf('(YYYY/MM/DD HH:MIN:SEC):\n  %04d/%02d/%02d  %02d:%02d:%02.3f\n', ...
-        YYYY, MM, DD, HH, MIN, SEC);
+        app.YYYY, app.MM, app.DD, app.HH, app.MIN, app.SEC);
 
 
 fprintf('\n--- Simulation Parameters ---\n');
-fprintf('K_d                        = %.4f\n', K_d);
-fprintf('K_p                        = %.4f\n', K_p);
-fprintf('K_w                        = %.0f\n', K_w);
-fprintf('Initial s/c angular rate   = [%.4f, %.4f, %.4f] deg/s\n', [wB_1; wB_2; wB_3]);
-fprintf('Initial quaternion         = [%.4f, %.4f, %.4f, %.4f]\n', [q1_ini; q2_ini; q3_ini; q4_ini]);
+fprintf('K_d                        = %.4f\n', app.K_d);
+fprintf('K_p                        = %.4f\n', app.K_p);
+fprintf('K_w                        = %.0f\n', app.K_w);
+fprintf('Initial s/c angular rate   = [%.4f, %.4f, %.4f] deg/s\n', [app.wB_1; app.wB_2; app.wB_3]);
+fprintf('Initial quaternion         = [%.4f, %.4f, %.4f, %.4f]\n', [app.q1_ini; app.q2_ini; app.q3_ini; app.q4_ini]);
 fprintf('Spacecraft inertia (kg/m2) =\n');
-fprintf('  %.4f  %.4f  %.4f\n', J_body(1,1), J_body(1,2), J_body(1,3));
-fprintf('  %.4f  %.4f  %.4f\n', J_body(2,1), J_body(2,2), J_body(2,3));
-fprintf('  %.4f  %.4f  %.4f\n', J_body(3,1), J_body(3,2), J_body(3,3));
+fprintf('  %.4f  %.4f  %.4f\n', app.J_body(1,1), app.J_body(1,2), app.J_body(1,3));
+fprintf('  %.4f  %.4f  %.4f\n', app.J_body(2,1), app.J_body(2,2), app.J_body(2,3));
+fprintf('  %.4f  %.4f  %.4f\n', app.J_body(3,1), app.J_body(3,2), app.J_body(3,3));
 
 
 % Helper function to get max magnitude value
@@ -269,3 +267,4 @@ fprintf('Maximum Angular Momentum Stored (each wheel)   = [%.4f %.4f %.4f] mN·m
 fprintf('Maximum Control Torque Applied (each wheel)    = [%.6f %.6f %.6f] mN·m\n', maxTorque);
 fprintf('Maximum Spin Rate (each wheel)                 = [%.3f %.3f %.3f] RPM\n', maxSpeed_w);
 
+end
